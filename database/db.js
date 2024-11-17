f262c1c93d1bb45691b1650b69293af41df365aa
Rohit Shekhar Kshirsagar <rohitkshi2000@gmail.com>
2024-11-17 23:45:03 +0530

    Add files via upload
    done

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 async function dbconnect() {
   var connection = await mongoose.connect(
-    "mongodb://127.0.0.1:27017/userdetails"
+    "mongodb://127.0.0.1:27017/productdb"
   );
   return connection;
 }
--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 var express = require("express");
 var mongoose = require("mongoose");
-var db = require("./database/db.js"); //database connection
 
-//connect mysql
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "cdac",
-});
-
-// console.log(db);
-//to connection use schema
+var db = require("./database/db.js");
+var app = express();
 db();
 
+//mongodb schema
 const Schema = mongoose.Schema;
 
-const userschema = new Schema({
-  name: String,
-  age: Number,
-  place: String,
+const productschema = new Schema({
+  pname: String,
+  price: Number,
+  discount: Number,
 });
 
 //connecting colection and schema
-const MyModel = mongoose.model("users", userschema);
-
-var app = express();
+const MyModel = mongoose.model("products", productschema);
 
 app.use(express.json());
 
-app.get("/users", async function (req, res) {
+app.get("/products", async function (req, res) {
   try {
     var result = await MyModel.find();
     res.send(result);
@@ -39,46 +28,14 @@ app.get("/users", async function (req, res) {
   }
 });
 
-app.post("/users", async function (req, res) {
-  //   console.log(req.body);
-
+app.post("/products", async function (req, res) {
   try {
     var record = new MyModel(req.body);
-    var ans = await record.save();
-    res.send("Record Inserted");
+    var result = await record.save();
+    res.send("Inserted data");
   } catch (err) {
     res.send(err.message);
   }
 });
 
-app.put("/users", function (req, res) {
-  res.send("Update data into database");
-});
-
-app.delete("/users", function (req, res) {
-  res.send("Deletee data into database");
-});
-
-//mysql databases
-app.get("/usersinfo", function (req, res) {
-  connection.query("select * from emp", function (err, data) {
-    if (err) {
-      res.send(err.message);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-app.post("/usersinfo", function (req, res) {
-  console.log(req.body);
-  connection.query("insert into emp set?", req.body, function (err, data) {
-    if (err) {
-      res.send(err.message);
-    } else {
-      res.send("Record Inserted");
-    }
-  });
-});
-
-app.listen(9000);
+app.listen(9200);
